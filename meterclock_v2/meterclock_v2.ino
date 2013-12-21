c21d78ad18e7abd33f08a6a66a0aa1f7dd37b4dd
Tim Bartlett <timbartlettpear@gmail.com>
2013-12-21 09:35:21 -0500

    bells and whistles added
    included faceMode to show minute marks, hourChime to add animation at
    the hour, and the hour hand now moves incrementally

--- meterclock_v2/meterclock_v2.ino
+++ meterclock_v2/meterclock_v2.ino
@@ -1,5 +1,5 @@
 // PixelStrip Clock
-// original design by Tim Bartlett
+// by Tim Bartlett
 // for 60 pixel strip and Teensy 3.0 with crystal
 
 #include <Time.h>
@@ -9,11 +9,18 @@
 // clockMode sets color scheme:
 // 0=GRAY, 1=BLUES, 2=ORANGES, 3=GREENS, 4=CHRISTMAS
 int clockMode = 1;
+// faceMode sets dial reference points:
+// 0=off, 1=every 15 minutes, 2=every 5 minutes
+int faceMode = 0;
+// hourChime adds blinky animation on the new hour, when true
+boolean hourChime = true;
 
 int sPrev;
 int mPrev;
 int hPrev;
 int hD;
+int twelveHour;
+int lastTH;
 int sR;
 int sG;
 int sB;
@@ -29,19 +36,23 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 void setup() {
   setSyncProvider(getTeensy3Time);
   mPrev = minute();
-  hPrev = hour();
+  hourFormat(hour());
+  hPrev = hD;
+  lastTH = twelveHour;
+  colorSelect(clockMode); // move to colorSelect input code below
   strip.begin();
   strip.show();
 }
 
 void loop() { 
-  // put input here to set clockMode
-  
-  colorSelect(clockMode);
+  // put input here to set clockMode 
+  // put input here to select faceMode
+ 
   eraseSecond(second());
   eraseMinute(minute());
   hourFormat(hour());
   eraseHour(hD);
+  setFace(faceMode);
   setClock();
   strip.show();
   delay(100);
@@ -53,10 +64,17 @@ time_t getTeensy3Time()
 }
 
 void hourFormat(int h) {
-   if (h < 12) {
-    hD = h * 5;
-  } else {
-    hD = (h - 12) * 5;
+  if (h == 0 || h == 12) {
+    twelveHour = 12;
+    hD = 0 + (minute() / 12);
+  } 
+  else if (h > 0 && h < 12) {
+    twelveHour = h; 
+    hD = (h * 5) + (minute() / 12);
+  } 
+  else {
+    twelveHour = h - 12;
+    hD = ((h - 12) * 5) + (minute() / 12);
   } 
 }
 
@@ -81,7 +99,35 @@ void eraseHour(int h) {
   if (h != hPrev) {
     strip.setPixelColor(hPrev, 0, 0, 0);
     hPrev = h;
-  } 
+  }
+  
+  if (twelveHour != lastTH && hourChime == false) {
+    strip.setPixelColor(hPrev, 0, 0, 0);
+    hPrev = h;
+  }
+  else if (twelveHour != lastTH && hourChime == true) {
+    for (int i = 0; i < twelveHour; i++) {
+      for (int p = 0; p < 60; p++) {
+        if (p <= h) {
+          strip.setPixelColor(p, mR, mG, mB);
+        }
+        else {
+          strip.setPixelColor(p, hR, hG, hB);
+        }
+        strip.show();
+        delay(17);
+      }     
+      for (int p = 0; p < 60; p++) {
+        strip.setPixelColor(p, 0, 0, 0);
+        strip.show();
+        delay(17);
+      }   
+    }
+    strip.setPixelColor(hPrev, 0, 0, 0);
+    hPrev = h;
+  }
+  else {
+  }
 }
 
 void setClock() {
@@ -107,18 +153,46 @@ void setClock() {
   }
 }
 
+void setFace(int ticks) {
+  switch(ticks){
+    case 0: //OFF
+      break;
+    
+    case 1: //every 15 minutes
+      strip.setPixelColor(0, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(15, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(30, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(45, (hR/50), (hG/50), (hB/50));
+      break;
+    
+    case 2: //every 5 minutes
+      strip.setPixelColor(0, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(5, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(10, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(15, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(20, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(25, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(30, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(35, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(40, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(45, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(50, (hR/50), (hG/50), (hB/50));
+      strip.setPixelColor(55, (hR/50), (hG/50), (hB/50));  
+  } 
+}
+
 void colorSelect(int mode) {
   switch(mode){
     case 0: //GRAY
-      sR = 100;
-      sG = 100;
-      sB = 100;
-      mR = 50;
-      mG = 50;
-      mB = 50;
-      hR = 25;
-      hG = 25;
-      hB = 25;
+      sR = 10;
+      sG = 10;
+      sB = 10;
+      mR = 255;
+      mG = 255;
+      mB = 255;
+      hR = 100;
+      hG = 100;
+      hB = 100;
       break;
       
     case 1: //BLUES
@@ -151,7 +225,7 @@ void colorSelect(int mode) {
       sB = 100;
       mR = 100;
       mG = 255;
-      mB = 100;
+      mB = 50;
       hR = 25;
       hG = 255;
       hB = 25;
